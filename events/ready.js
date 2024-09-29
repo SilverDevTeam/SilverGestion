feb77c6e12bb027b9863204eabf45cfd339ef5ad
x1 <https://dsc.gg/silver-gestion>
2024-09-29 21:30:51 +0200

    Add d'un message de starting

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,18 @@ module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
+        const embed = new EmbedBuilder()
+            .setTitle(`Je suis oppérationel !`)
+            .setDescription(`🏓 **Mon ping est de :** ${client.ws.ping} ms. \n<:member:1262160675686584414> J'ai **${client.guilds.cache.size} serveurs** et **${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)} utilisateurs !**`)
+            .setColor('Green')
+            .setTimestamp()
+            .setFooter({
+                text: `La console virtuelle !`,
+                iconURL: client.user.displayAvatarURL(),
+            });
+
+        client.channels.cache.get('1290031426904785029').send({ embeds: [embed] })
+
         console.log(`[READY] ${client.user.tag} est prêt | ${client.guilds.cache.size} serveurs | ${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)} utilisateurs`);
         setInterval(() => {
             let random = Math.floor(Math.random() * status.length);
