6bad1acb49788419c9b69094c9edd2fce3ec5de9
*x1 <https://dsc.gg/xx1>
2024-09-29 18:37:07 +0200

    Add snipe.js

---
+++ commands/snipe.js
@@ -0,0 +1,56 @@
+const { EmbedBuilder, PermissionFlagsBits, PermissionsBitField } = require('discord.js');
+const db = require('../../fonctions/database.js');
+
+module.exports = {
+    name: "snipe",
+    description: "Afficher le dernier message suppimer.",
+    aliases: [],
+    permissions: [PermissionsBitField.Flags.UseApplicationCommands],
+    guildOwnerOnly: false,
+    botOwnerOnly: false,
+
+    async execute(message, args, client) {
+        const donnee = await new Promise((resolve, reject) => {
+            db.get(`SELECT * FROM channels WHERE guildId = ? AND channelId = ?`, [message.guild.id, message.channel.id], (err, row) => {
+                if (err) reject(err);
+                resolve(row);
+            });
+        });
+        if (donnee.snipe == null) return message.channel.send('> **<:modo:1262161126414614590> Aucun message supprimé**')
+        const embed = new EmbedBuilder()
+            .setTitle('Snipe :')
+            .setColor('Red')
+            .setTimestamp()
+            .setDescription(`
+                __**Message :**__ ${donnee.snipe}
+                
+                __**Auteur :**__ <@${donnee.snipeAuthor}>
+            `)
+        message.channel.send({ embeds: [embed] })
+    },
+    async executeSlash(client, interaction) {
+        const donnee = await new Promise((resolve, reject) => {
+            db.get(`SELECT * FROM channels WHERE guildId = ? AND channelId = ?`, [interaction.guild.id, interaction.channel.id], (err, row) => {
+                if (err) reject(err);
+                resolve(row);
+            });
+        });
+        if (donnee.snipe == null) return interaction.channel.send('> **<:modo:1262161126414614590> Aucun message supprimé**')
+        const embed = new EmbedBuilder()
+            .setTitle('Snipe :')
+            .setColor('Red')
+            .setTimestamp()
+            .setDescription(`
+                __**Message :**__ ${donnee.snipe}
+                
+                __**Auteur :**__ <@${donnee.snipeAuthor}>
+            `)
+        interaction.reply({ embeds: [embed] })
+    },
+    get data() {
+        return new SlashCommandBuilder()
+            .setName(this.name)
+            .setDescription(this.description)
+            .setDefaultMemberPermissions(PermissionFlagsBits.UseApplicationCommands)
+    }
+}
