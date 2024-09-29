f89b266aa05db9e808c037cc8fea80234362913d
x1 <https://dsc.gg/silver-gestion>
2024-09-29 22:44:20 +0200

    Update / Desactivation du systeme

--- events/snipe.js
+++ events/snipe.js
@@ -1,15 +1,15 @@
-const { EmbedBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder, Events, AttachmentBuilder } = require('discord.js');
+const { EmbedBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder, AttachmentBuilder } = require('discord.js');
 const db = require('../fonctions/database.js');
 
 module.exports = {
-  name: Events.MessageDelete,
-  async execute(client, message) {
-    if (message.author.bot) return;
-    if (message.content.startsWith(client.config.prefix)) { return }
-    
+  name: "messageDelete",
+  async execute(message, client) {
+    /*if (message.channel.isDMBased() || message.author.bot) return;
+    if (message.content.startsWith(client.config.prefix)) return;
+
     db.run(
       `UPDATE channels SET snipe = ?, snipeAuthor = ? WHERE guildId = ? AND channelId = ?`,
       [message.content, message.author.id, message.guild.id, message.channel.id],
-    );
+    );*/
   }
 };
