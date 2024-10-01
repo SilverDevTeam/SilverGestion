851095f1a3465d208935929225174780d2d5b45e
Luox <130280834+luoxthedev@users.noreply.github.com>
2024-10-01 15:07:50 +0200

    Update deploycommand.js

--- deploycommand.js
+++ deploycommand.js
@@ -1,7 +1,6 @@
-const { Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10'); // Make sure you're importing this properly
 const fs = require('fs');
-
 const shadow = require("./shadow.json");
 const config = require("./config.json");
 const clientId = config.id;
@@ -17,13 +16,19 @@ commandFiles.forEach(commandFile => {
 const rest = new REST({ version: '10' }).setToken(shadow.token);
 
 rest.put(
-    // Routes.applicationGuildCommands(clientId, guildId), { body: commands } // Guild commands
+    // Routes.applicationGuildCommands(clientId, guildId), { body: commands } // Uncomment this if you are registering guild-specific commands
     Routes.applicationCommands(clientId), { body: commands } // Global commands
 )
-    .then((data) => {
-        console.log(`Successfully registered ${data.length} application commands.`);
-        const { exec } = require('child_process');
-        exec('node index.js');
-        catch(console.error);
-        });
-
+.then((data) => {
+    console.log(`Successfully registered ${data.length} application commands.`);
+    const { exec } = require('child_process');
+    exec('node index.js', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing index.js: ${error}`);
+            return;
+        }
+        if (stdout) console.log(`Output: ${stdout}`);
+        if (stderr) console.error(`Error Output: ${stderr}`);
+    });
+})
+.catch(console.error);
