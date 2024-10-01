54c399a829db0c1be02b7362e1e7ce8026507e36
x1 <https://dsc.gg/silver-gestion>
2024-10-01 21:14:45 +0200

    Update snipe.js

--- events/snipe.js
+++ events/snipe.js
@@ -4,12 +4,14 @@ const db = require('../fonctions/database.js');
 module.exports = {
   name: "messageDelete",
   async execute(client, message) {
-    /*if (message.channel.isDMBased() || message.author.bot) return;
+    if (message.channel.isDMBased()) return;
+    if (message.author.bot) return;
+    if (message.system) return;
     if (message.content.startsWith(client.config.prefix)) return;
 
     db.run(
       `UPDATE channels SET snipe = ?, snipeAuthor = ? WHERE guildId = ? AND channelId = ?`,
       [message.content, message.author.id, message.guild.id, message.channel.id],
-    );*/
+    );
   }
 };
