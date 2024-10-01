c3be17eff67a72e1b9ba73cd0cfb4af3b38cc4b7
x1 <https://dsc.gg/silver-gestion>
2024-10-01 16:44:23 +0200

    Add say.js

---
+++ commands/say.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, PermissionsBitField, EmbedBuilder } = require("discord.js");
+const db = require("../fonctions/database.js");
+
+module.exports = {
+    name: "say",
+    description: "Envoie un message sous le nom du bot.",
+    aliases: [],
+    permissions: [PermissionsBitField.Flags.ViewChannel],
+    guildOwnerOnly: false,
+    botOwnerOnly: false,
+    async execute(client, message, args) {
+        let msg = ""
+        for (let i = 0; i < args.length; i++) {
+          msg += args[i] + ' '
+        }
+        message.channel.send(msg)
+    },
+    async executeSlash(client, interaction) {
+        interaction.channel.send(interaction.options.getString('message'))
+    },
+    get data() {
+        return new SlashCommandBuilder()
+            .setName(this.name)
+            .setDescription(this.description)
+            .addStringOption(option =>
+                option.setName('message')
+                    .setDescription("Message que je dois envoyer")
+                    .setRequired(true)
+            )
+    }
+}
