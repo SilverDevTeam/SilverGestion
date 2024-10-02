6350804092391f5799603676a7c9321b9a145dae
x1 <https://dsc.gg/silver-gestion>
2024-10-02 08:37:40 +0200

    Update snipe (fix tempo)

--- events/snipe.js
+++ events/snipe.js
@@ -4,14 +4,17 @@ const db = require('../fonctions/database.js');
 module.exports = {
   name: "messageDelete",
   async execute(client, message) {
-    if (message.channel.isDMBased()) return;
-    if (message.author.bot) return;
-    if (message.system) return;
-    if (message.content.startsWith(client.config.prefix)) return;
+    try {
+      if (message.channel.isDMBased()) return;
+      if (message.author.bot) return;
+      if (message.system) return;
+      if (message.content.startsWith(client.config.prefix)) return;
 
-    db.run(
-      `UPDATE channels SET snipe = ?, snipeAuthor = ? WHERE guildId = ? AND channelId = ?`,
-      [message.content, message.author.id, message.guild.id, message.channel.id],
-    );
+      db.run(
+        `UPDATE channels SET snipe = ?, snipeAuthor = ? WHERE guildId = ? AND channelId = ?`,
+        [message.content, message.author.id, message.guild.id, message.channel.id],
+      );
+    }
+    catch { return }
   }
 };
