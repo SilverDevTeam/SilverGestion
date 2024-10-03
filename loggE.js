cda745a3a0cb1c9fc0d74e07acb5d2c6ebb5ee8f
Luox <130280834+luoxthedev@users.noreply.github.com>
2024-10-03 09:16:58 +0000

    Changelent d'orthographe

--- loggE.js
+++ loggE.js
@@ -25,7 +25,7 @@ module.exports = function loggE(client, erreur) {
         .setColor('Red')
         .setTimestamp()
         .setFooter({
-            text: `La console virtuelle !`,
+            text: `CONSOLE`,
             iconURL: client.user.displayAvatarURL(), 
         });
 
