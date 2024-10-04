9e20a79b03b2195b5f3967866ab3d3f616f72a15
x1 <https://dsc.gg/silver-gestion>
2024-10-04 18:08:37 +0200

    Add logsmessage in snipe.js

--- events/snipe.js
+++ events/snipe.js
@@ -15,6 +15,29 @@ module.exports = {
         [message.content, message.author.id, message.guild.id, message.channel.id],
       );
     }
-    catch { return }
+    catch { }
+
+    // LOGS
+    const donnee = await new Promise((resolve, reject) => {
+      db.get(`SELECT * FROM logs WHERE guildId = ?`, [message.guild.id], (err, row) => {
+        if (err) reject(err);
+        resolve(row);
+      });
+    });
+    if (donnee.logsmessage) {
+      try {
+        const embed = new EmbedBuilder()
+          .setTitle(`Message supprimé`)
+          .setDescription(`**Utilisateur : <@${message.author.id}> | \`${message.author.tag}\`**\nContenu du message :\n\n` + message.content)
+          .setColor('Red')
+          .setTimestamp()
+          .setFooter({
+            text: ` `,
+            iconURL: client.user.displayAvatarURL(),
+          });
+
+        client.channels.cache.get(donnee.logsmessage).send({ embeds: [embed] })
+      } catch { }
+    }
   }
 };
