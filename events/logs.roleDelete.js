8ab92abe4f7c5e85e0c1ef4a9a2a59dcdf67a9fb
x1 <https://dsc.gg/silver-gestion>
2024-10-07 17:23:34 +0200

    Add logsChannelDelete

---
+++ events/logs.roleDelete.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder, AttachmentBuilder } = require('discord.js');
+const db = require('../fonctions/database.js');
+
+module.exports = {
+  name: "roleDelete",
+  async execute(client, role) {
+
+    // LOGS
+    const donnee = await new Promise((resolve, reject) => {
+      db.get(`SELECT * FROM logs WHERE guildId = ?`, [role.guild.id], (err, row) => {
+        if (err) reject(err);
+        resolve(row);
+      });
+    });
+    if (donnee.logsserveur) {
+      try {
+        const embed = new EmbedBuilder()
+          .setTitle(`Rôle supprimé`)
+          .setDescription(`**Nom du rôle : ${role.name}**`)
+          .setColor('Red')
+          .setTimestamp()
+          .setFooter({
+            text: ` `,
+            iconURL: client.user.displayAvatarURL(),
+          });
+
+        client.channels.cache.get(donnee.logsserveur).send({ embeds: [embed] })
+      } catch { }
+    }
+  }
+};
